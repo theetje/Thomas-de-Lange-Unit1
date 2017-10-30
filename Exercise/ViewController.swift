226a1730050449ff4b10b828daa953aff3aebec6
theetje <delange.thomas@gmail.com>
2017-10-30 12:53:30 +0100

    Initial Commit

---
+++ Exercise/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Exercise
+//
+//  Created by Thomas De lange on 30-10-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
