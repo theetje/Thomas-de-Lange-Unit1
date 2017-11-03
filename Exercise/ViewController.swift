df5ce8d184ec0d9fe24b2fc3e5d78c1e9589c37d
theetje <delange.thomas@gmail.com>
2017-11-03 12:47:42 +0100

    Nieuwe knop aan gemaakt gaat aan en uit.

--- Exercise/ViewController.swift
+++ Exercise/ViewController.swift
@@ -8,9 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var lightOn = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +22,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonPressed(_ sender: AnyObject) {
+        lightOn = !lightOn
+        updateUI()
+    }
+    
+    func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
+    }
+    
 }
 
